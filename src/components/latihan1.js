2de0e5e412f487ccd594edccc1f673ce824be7d6
hanhein <hh.heinrichansen@gmail.com>
2020-10-27 16:35:04 +0700

    remed

--- src/components/latihan1.js
+++ src/components/latihan1.js
@@ -1,5 +1,66 @@
 import React from 'react'
+import Database from './database'
+
 class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          items:[],
+          currentItem:{
+            text:'',
+            key:''
+          }
+        }
+        this.addItem = this.addItem.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.setUpdate = this.setUpdate.bind(this);
+      }
+      addItem(e){
+        e.preventDefault();
+        const newItem = this.state.currentItem;
+        if(newItem.text !==""){
+          const items = [...this.state.items, newItem];
+        this.setState({
+          items: items,
+          currentItem:{
+            text:'',
+            key:''
+          }
+        })
+        }
+      }
+      handleInput(e){
+        this.setState({
+          currentItem:{
+            text: e.target.value,
+            key: Date.now()
+          }
+        })
+      }
+      deleteItem(key){
+        const filteredItems= this.state.items.filter(item =>
+          item.key!==key);
+        this.setState({
+          items: filteredItems
+        })
+    
+      }
+      setUpdate(text,key){
+        console.log("items:"+this.state.items);
+        const items = this.state.items;
+        items.map(item=>{      
+          if(item.key===key){
+            console.log(item.key +"    "+key)
+            item.text= text;
+          }
+        })
+        this.setState({
+          items: items
+        })
+        
+       
+    }
     render(){
         return(
             <div>
@@ -25,11 +86,15 @@ class Home extends React.Component{
                     </tbody>
                 </table>
                 <div className='row'>
-                    <div className='col-md-3'> <input type='text' className='form-control' placeholder='Nama' /> </div>
+                    <div className='col-md-3'> <input type='text' className='form-control' placeholder='Nama'
+                    value={this.state.currentItems.text} onSubmit={this.addItem} onChange ={this.currentItem}/> </div>
                     <div className='col-md-3'> <input type='text' className='form-control' placeholder='Usia' /> </div>
                     <div className='col-md-3'> <input type='text' className='form-control' placeholder='Pekerjaan' /> </div>
                     <div className='col-md-3'> <input type='button' className='form-control btn-info' value='add Data' /> </div>
                 </div>
+                <p>{this.state.items.text}</p>
+        
+                <Database items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
             </div>
         )
     }
